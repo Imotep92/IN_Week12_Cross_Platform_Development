ad708e3d263177ee711ae13a21c001545732e7d1
Imotep92 <Imogyn.nicholas@yahoo.co.uk>
2025-07-22 16:27:35 +0100

    Desktop input Branch, 22/7/25, 16:27pm
    in Unity Editor:
    - added potion bottle png
    
    in VSCode:
    - added a potion bottle scriptable object script

---
+++ IN_Week12_Cross_Platform_Dev/Assets/Scripts/potionBottleInfoScript.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Potion Infomation", fileName = "new Potion Info")]
+public class potionBottleInfoScript : ScriptableObject //Scriptable Object
+{
+   
+    public string potionName;
+    public string potionDescription;
+    public Sprite potionSprite;
+
+   // public int armourLevel;
+    //public float npcSpeed;
+
+   // public bool isFriendly;
+}
